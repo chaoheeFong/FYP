e03d2f87281a8ebe841d9ad2696e49c2dd0261aa
chaoheeFong <chaohee00@gmail.com>
2023-06-24 12:19:19 +0800

    Create File
    Migration, Controller, Model

--- RTBS-Cat-Boarding-Shop/database/migrations/2023_06_24_025041_create_booking_table.php
+++ RTBS-Cat-Boarding-Shop/database/migrations/2023_06_24_025041_create_booking_table.php
@@ -11,12 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('admins', function (Blueprint $table) {
+        Schema::create('booking', function (Blueprint $table) {
             $table->id();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->date();
             $table->timestamps();
         });
     }
@@ -26,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('booking');
     }
 };
